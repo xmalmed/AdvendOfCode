9e75ca3d93db3d83f48ff5ebb02023d489d6c17e
xmalmed <vojtech.kuban@solarwinds.com>
2020-12-07 22:11:21 +0100

    puzzle 5

--- AoC_2020/puzzle_5/puzzle_5.py
+++ AoC_2020/puzzle_5/puzzle_5.py
@@ -1,6 +1,21 @@
+def parse_input(text: str):
+    int_2 = text.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
+    return int(int_2, 2)
+
+
 def load_input():
-    file_lines = []
-    with open("input_4.txt", 'r') as lines:
+    data = []
+    with open("input_5.txt", 'r') as lines:
         for line in lines:
-            file_lines.append(line.strip('\n'))  # replace(':', '') replace('-', ' ')) split()
-    return file_lines
+            data.append(parse_input(line.strip('\n')))
+    return data
+
+
+if __name__ == "__main__":
+    seats = sorted(load_input())
+    previous = seats[0] - 1
+    print(f"max {seats[-1]}")
+    for seat in seats:
+        if seat - previous > 1:
+            print(f"previous: {previous}, next {seat}")
+        previous = seat
