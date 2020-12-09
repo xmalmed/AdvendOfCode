d44ab7e47a63023117205fbc02f8cdd7be975f2b
xmalmed <vojtech.kuban@solarwinds.com>
2020-12-09 13:06:12 +0100

    7 puzzle part 2

--- AoC_2020/puzzle_7/puzzle_7.py
+++ AoC_2020/puzzle_7/puzzle_7.py
@@ -1,10 +1,13 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+from time import time
+imports = time()
+# import networkx as nx
+from networkx.algorithms.dag import ancestors
+from networkx import DiGraph
+# import matplotlib.pyplot as plt
 from re import match, findall
-import pygraphviz
-import pydot
-from networkx.drawing.nx_agraph import write_dot
-import scipy
+# import pygraphviz
+# import pydot
+# from networkx.drawing.nx_agraph import write_dot
 
 
 def load_input(filename: str):
@@ -24,27 +27,49 @@ def parse_input(text: str):
     return bag, contains_bags
 
 
-# bags, contain = load_input("input_7.txt")
+def count_of_subbags(bag: str ="shiny gold", total: int = 0) -> int:
+    count_subbags = 0
+    edges = G.out_edges(bag)
+
+    if len(edges):
+        for edge in edges:
+            count_subbags += G.get_edge_data(*edge)['weight'] * count_of_subbags(edge[1])
+    else:
+        return 1 #count_subbags += G.get_edge_data(*edge)['weight']
+
+    return count_subbags + 1
+
+
+start = time()
+# bags, contain = load_input("input_test_7.txt")
+# bags, contain = load_input("input_test2_7.txt")
 bags, contain = load_input("input_7.txt")
 
-G = nx.DiGraph()
+loading = time()
+
+# G = nx.DiGraph()
+G = DiGraph()
 G.add_nodes_from(bags)
 for i, con in enumerate(contain):
     G.add_edges_from([(bags[i], c[1], {"weight": int(c[0])}) for c in con])
-nx.draw(G, with_labels=True)
-#
-# plt.subplot(122)
-# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-A = nx.nx_agraph.to_agraph(G)  # convert to a graphviz graph
-# X1 = nx.nx_agraph.from_agraph(A)  # convert back to networkx (but as Graph)
-# X2 = nx.Graph(A)  # fancy way to do conversion
-# G1 = nx.Graph(X1)  # now make it a Graph
 
-# A.write("k5.dot")  # write to dot file
-# write_dot(A, "grid.dot")
-A.layout('dot')
-A.draw("bags_graph.svg")
+# super_bags = nx.algorithms.dag.ancestors(G, "shiny gold")
+super_bags = ancestors(G, "shiny gold")
+# print(super_bags)
+print(f'Number of super bags: {len(super_bags)}')
 
-# plt.show()
+total_bags = count_of_subbags()
+print(f'Total number bags in shiny gold: {total_bags - 1}') # -1 for shiny gold
+
+end = time()
+print(f"loading input: {loading - start}, solving: {end - loading}")
+print(f"imports: {start - imports}")
 
 
+# needs uncomment some of the imports
+# def plot_graph(G):
+#     # Write a plot to DOT format and plot graph by Graphviz.
+#     A = nx.nx_agraph.to_agraph(G)  # convert to a graphviz graph
+#     A.write("bags.dot")  # write to dot file
+#     A.layout('dot')  # graph style
+#     A.draw("bags_graph.svg")
