0b5f7251291842d145b89c0b1b2ed8e6b0791d20
Vojta Kuban <xmalmed@gmail.com>
2021-12-02 09:32:07 +0100

    puzzle 2

--- AoC_2021/puzzle_2/puzzle_2.py
+++ AoC_2021/puzzle_2/puzzle_2.py
@@ -1,5 +1,5 @@
-import copy
 from parse import parse
+from numpy import array
 
 
 if __name__ == "__main__":
@@ -10,7 +10,27 @@ if __name__ == "__main__":
 
     with open(filename, "r") as input_file:
         for line in input_file:
-            (i, o) = parse("{} -> {}", line.strip())
-            data.append((i, o))
+            (d, n) = parse("{} {:d}", line.strip())
+            data.append((d, n))
 
-    # data2 = deepcopy(data)
+    print("total forward is: ", f := sum(n[1] for n in filter(lambda x: x[0] == "forward", data)))
+    print(
+        "total depth is: ",
+        d := sum(n[1] for n in filter(lambda x: x[0] == "down", data))
+        - sum(n[1] for n in filter(lambda x: x[0] == "up", data)),
+    )
+    print("multiply:", d*f)
+
+    # part 2
+    aim = 0
+    position = array([0, 0])
+    for i in data:
+        if i[0] == "forward":
+            position += array([i[1], i[1] * aim])
+        elif i[0] == "up":
+            aim -= i[1]
+        elif i[0] == "down":
+            aim += i[1]
+
+    print("new position is: ", position)
+    print("mutliply: ", position[0] * position[1])
