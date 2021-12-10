6987c413296bf97093a5aee1fe681a9e072a1178
malmed <xmalmed@gmail.com>
2021-12-10 15:09:18 +0100

    add days 10

--- AoC_2021/puzzle_10/puzzle_10.py
+++ AoC_2021/puzzle_10/puzzle_10.py
@@ -1,7 +1,52 @@
 from utils.puzzle import Puzzle
 
+PAIRS = {")": "(", "]": "[", "}": "{", ">": "<"}
+PAIRS2 = {PAIRS[k]: k for k in PAIRS}
+open_brackets = PAIRS.values()
 
-if __name__ == '__main__':
+VALUES = {")": 3, "]": 57, "}": 1197, ">": 25137}
+VALUES2 = {"(": 1, "[": 2, "{": 3, "<": 4}
+
+
+def validate_line(line):
+    lifo = []
+    for bracket in line:
+        if (len(lifo) == 0) or (bracket in open_brackets):
+            lifo.append(bracket)
+        else:
+            if PAIRS[bracket] == lifo[-1]:
+                lifo.pop(-1)
+            else:
+                return VALUES[bracket]
+
+    return lifo
+
+
+def autocomplete(lifo):
+    autocomplete_scores = 0
+    while lifo:
+        autocomplete_scores *= 5
+        autocomplete_scores += VALUES2[lifo.pop()]
+    return autocomplete_scores
+
+
+if __name__ == "__main__":
     p = Puzzle()
     data = p.load_input()
-    data = p.load_input('input_test.txt')
+    # data = p.load_input("input_test.txt")
+
+    total_error = 0
+    autocomplete_scores = []
+    for line in data:
+        res = validate_line(line)
+        if isinstance(res, int):
+            total_error += res
+        else:
+            autocomplete_scores.append(autocomplete(res))
+
+    print("Total syntax error: ", total_error)
+
+    autocomplete_scores.sort()
+    print(
+        "Autocomplete score: ", autocomplete_scores[int((len(autocomplete_scores) - 1) / 2)]
+    )
