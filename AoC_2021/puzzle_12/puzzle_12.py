317c725b31a82b0565836ad086ed74d48da0e818
malmed <xmalmed@gmail.com>
2021-12-12 23:30:38 +0100

    day 12, the second part

--- AoC_2021/puzzle_12/puzzle_12.py
+++ AoC_2021/puzzle_12/puzzle_12.py
@@ -4,24 +4,33 @@ from parse import parse
 
 class Cave:
     def __init__(self, name):
-        # if name.isupper():
         self.name = name
         self.neighbours = []
 
-    # def neighbours(self):
-    #     pass
-    #
-    # def add_neighbour(self, n):
+
+def find_all_paths(start_path):
+    paths = start_path
+    end_paths = set()
+    while len(paths) > 0:
+        path = paths.pop(0)
+        for n in caves[path[-1]].neighbours:
+            if n.name == "end":
+                end_paths.add("-".join(path + [n.name]))
+            elif n.name == "start":
+                continue
+            elif (n.name.islower() and n.name not in path) or n.name.isupper():
+                paths.append(path + [n.name])
+    return end_paths
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     p = Puzzle()
     data = p.load_input()
     # data = p.load_input('input_test.txt')
 
     caves = {}
     for line in data:
-        a, b = parse('{}-{}', line)
+        a, b = parse("{}-{}", line)
         if not a in caves:
             caves[a] = Cave(a)
         if not b in caves:
@@ -29,19 +38,26 @@ if __name__ == '__main__':
         caves[a].neighbours.append(caves[b])
         caves[b].neighbours.append(caves[a])
 
-    # for c in caves.values():
-    #     print(c.name, [n.name for n in c.neighbours])
+    paths = [["start"]]
+    # part 1
+    end_paths = find_all_paths(paths)
+    print(len(end_paths))
 
-    paths = [['start']]
+    # part 2
+    paths = [["start"]]
+    second_paths = []
     end_paths = set()
     while len(paths) > 0:
         path = paths.pop(0)
         for n in caves[path[-1]].neighbours:
-            if n.name == 'end':
-                end_paths.add('-'.join(path + [n.name]))
+            if n.name == "end":
+                end_paths.add("-".join(path + [n.name]))
+            elif n.name == "start":
+                continue
             elif (n.name.islower() and n.name not in path) or n.name.isupper():
                 paths.append(path + [n.name])
+            else:
+                second_paths.append(path + [n.name])
 
-    for e in end_paths:
-        print(e)
-    print(len(end_paths))
+    end_paths2 = find_all_paths(second_paths)
+    print(len(end_paths2) + len(end_paths))
