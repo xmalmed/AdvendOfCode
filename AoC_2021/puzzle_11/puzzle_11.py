d5fcdfddb43559ffe18295580c1048f506cd4b14
malmed <xmalmed@gmail.com>
2021-12-12 23:12:33 +0100

    add days 11 and 1/2 of day 12

--- AoC_2021/puzzle_11/puzzle_11.py
+++ AoC_2021/puzzle_11/puzzle_11.py
@@ -1,7 +1,52 @@
 from utils.puzzle import Puzzle
+from numpy import array
+from itertools import product
+
+edge = float('-inf')
+surrounding = list(product([-1, 0, 1], [-1, 0, 1]))
+surrounding.remove((0, 0))
+
+def charge_surroundings(x, y):
+    for s in surrounding:
+        if octopuses[y + s[0], x + s[1]] > 0:
+            octopuses[y + s[0], x + s[1]] += 1
 
 
 if __name__ == '__main__':
     p = Puzzle()
-    data = p.load_input()
-    data = p.load_input('input_test.txt')
+    data = p.load_input(string=True)
+    # data = p.load_input('input_test.txt', string=True)
+
+
+    octopuses = [[edge]*12]
+    for line in data:
+        octopuses.append([edge] + [int(ch) for ch in line] + [edge])
+    octopuses.append([edge]*12)
+    octopuses = array(octopuses)
+    #
+    # print('Step 0')
+    # print(octopuses)
+
+    total_flashes = 0
+    for step in range(1, 100000):
+        print('Step: ', step)
+        octopuses += 1
+        # print(octopuses)
+        flash = True
+        while flash:
+            # charged = octopuses[octopuses >= 9]
+            flash = False
+            for y in range(1, 11):
+                for x in range(1, 11):
+                    if octopuses[y, x] >= 10:
+                        octopuses[y, x] = 0
+                        charge_surroundings(x, y)
+                        total_flashes += 1
+                        flash = True
+
+        if len(octopuses[octopuses >= 1]) == 0:
+            print("Synchronous step: ", step)
+            break
+
+        if step == 100:
+            print(total_flashes)
--- AoC_2021/puzzle_12/puzzle_12.py
+++ AoC_2021/puzzle_12/puzzle_12.py
@@ -1,7 +1,47 @@
 from utils.puzzle import Puzzle
+from parse import parse
+
+
+class Cave:
+    def __init__(self, name):
+        # if name.isupper():
+        self.name = name
+        self.neighbours = []
+
+    # def neighbours(self):
+    #     pass
+    #
+    # def add_neighbour(self, n):
 
 
 if __name__ == '__main__':
     p = Puzzle()
     data = p.load_input()
-    data = p.load_input('input_test.txt')
+    # data = p.load_input('input_test.txt')
+
+    caves = {}
+    for line in data:
+        a, b = parse('{}-{}', line)
+        if not a in caves:
+            caves[a] = Cave(a)
+        if not b in caves:
+            caves[b] = Cave(b)
+        caves[a].neighbours.append(caves[b])
+        caves[b].neighbours.append(caves[a])
+
+    # for c in caves.values():
+    #     print(c.name, [n.name for n in c.neighbours])
+
+    paths = [['start']]
+    end_paths = set()
+    while len(paths) > 0:
+        path = paths.pop(0)
+        for n in caves[path[-1]].neighbours:
+            if n.name == 'end':
+                end_paths.add('-'.join(path + [n.name]))
+            elif (n.name.islower() and n.name not in path) or n.name.isupper():
+                paths.append(path + [n.name])
+
+    for e in end_paths:
+        print(e)
+    print(len(end_paths))
