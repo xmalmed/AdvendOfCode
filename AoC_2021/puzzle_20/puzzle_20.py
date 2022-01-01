369b8d7a8bc2ed9422e767ba48f90b0d30a31f17
malmed <xmalmed@gmail.com>
2022-01-01 02:01:30 +0100

    Day 20 and 24

--- AoC_2021/puzzle_20/puzzle_20.py
+++ AoC_2021/puzzle_20/puzzle_20.py
@@ -1,7 +1,55 @@
 from utils.puzzle import Puzzle
 
 
-if __name__ == '__main__':
+def get_space_value(last_space):
+    if iea[0] == 0:
+        return 0
+    elif iea[-1] == 1:
+        return 1
+    else:
+        return (last_space + 1) % 2
+
+
+def get_number(x, y, image, default):
+    num = []
+    for dy in range(-1, 2):
+        for dx in range(-1, 2):
+            if (x + dx, y + dy) in image:
+                num.append(str(image[(x + dx, y + dy)]))
+            else:
+                num.append(str(default))
+    v = int("".join(num), 2)
+    return v
+
+
+if __name__ == "__main__":
     p = Puzzle()
     data = p.load_input()
-    data = p.load_input('input_test.txt')
+    # data = p.load_input("input_test.txt")
+    iea = [1 if ch == "#" else 0 for ch in data[0]]
+
+    images = []
+    for j in range(0, 51):
+        images.append({})
+
+    for y, line in enumerate(data[2:]):
+        for x, ch in enumerate(line):
+            if ch == "#":
+                images[0][(x, y)] = 1
+
+    size_x = x + 1
+    size_y = y + 1
+
+    space = 0
+    for i in range(1, 51):
+        new_space = get_space_value(space)
+        for y in range(0 - i, size_y + i):
+            for x in range(0 - i, size_x + i):
+                value = iea[get_number(x, y, images[i - 1], space)]
+                if value != new_space:
+                    images[i][(x, y)] = value
+        space = new_space
+        # print_test_image(image)
+        if i == 3:
+            print(len(images[i]))
+    print(len(images[50]))
--- AoC_2021/puzzle_24/puzzle_24.py
+++ AoC_2021/puzzle_24/puzzle_24.py
@@ -1,7 +1,53 @@
 from utils.puzzle import Puzzle
 
 
-if __name__ == '__main__':
+def run_code(input):
+    mem = {"w": 0, "x": 0, "y": 0, "z": 0}
+    for line in program:
+        if line[0] == "inp":
+            mem[line[1]] = int(input.pop(0))
+        else:
+            value = mem[line[2]] if line[2] in mem else int(line[2])
+            if line[0] == "add":
+                mem[line[1]] += value
+            elif line[0] == "mul":
+                mem[line[1]] *= value
+            elif line[0] == "div":
+                mem[line[1]] = mem[line[1]] // value
+            elif line[0] == "mod":
+                mem[line[1]] = mem[line[1]] % value
+            elif line[0] == "eql":
+                mem[line[1]] = 1 if mem[line[1]] == value else 0
+    return mem
+
+
+if __name__ == "__main__":
     p = Puzzle()
     data = p.load_input()
-    data = p.load_input('input_test.txt')
+    # data = p.load_input('input_test.txt')
+
+    program = [line.split() for line in data]
+
+    for number in range(91_398_299_698_996, 11_111_111_111_111, -1):
+        input = list(str(number))
+        if "0" in input:
+            continue
+        else:
+            print(number)
+            mem = run_code(input)
+            if mem["z"] == 0:
+                print(number)
+                print(mem)
+                break
+
+    for number in range(41_171_183_141_291, 91_398_299_698_998, 1):
+        input = list(str(number))
+        if "0" in input:
+            continue
+        else:
+            print(number)
+            mem = run_code(input)
+            if mem["z"] == 0:
+                print(number)
+                print(mem)
+                break
