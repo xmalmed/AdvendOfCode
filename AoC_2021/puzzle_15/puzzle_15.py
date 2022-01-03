608a841952fb643f5fa1fabf06d1c715ffd53289
malmed <xmalmed@gmail.com>
2022-01-03 22:44:04 +0100

    Day 15, part 1 and 2

--- AoC_2021/puzzle_15/puzzle_15.py
+++ AoC_2021/puzzle_15/puzzle_15.py
@@ -1,7 +1,90 @@
 from utils.puzzle import Puzzle
+from numpy import zeros, append, fromstring
 
 
-if __name__ == '__main__':
+def next_nodes(size):
+    steps = []
+    for r in range(size):
+        step = []
+        for i in range(r + 1):
+            step.append((i, r - i))
+        steps.append(step)
+    for i, s in enumerate(steps[-2::-1]):
+        steps.append([(x[0] + i + 1, x[1] + i + 1) for x in s])
+    return steps
+
+
+def update_all_neighbours(node, maze, path, size, depth):
+    depth += 1
+    if depth >= 8:  # magic constant to prevent too many recursions
+        return
+    path_here = path[node]
+    for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        if 0 <= node[0] + dx < size and 0 <= node[1] + dy < size:
+            neighbour_node = (node[0] + dx, node[1] + dy)
+            neighbour_path = path[neighbour_node]
+            neighbour_node_distance = maze[neighbour_node]
+            if neighbour_path > neighbour_node_distance + path_here:
+                path[neighbour_node] = neighbour_node_distance + path_here
+                update_all_neighbours(neighbour_node, maze, path, size, depth)
+
+
+def extend_neighbours(node, maze, path, size):
+    path_here = path[node]
+    for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+        if 0 <= node[0] + dx < size and 0 <= node[1] + dy < size:
+            neighbour_node = (node[0] + dx, node[1] + dy)
+            neighbour_path = path[neighbour_node]
+            neighbour_node_distance = maze[neighbour_node]
+            if neighbour_path > neighbour_node_distance + path_here:
+                path[neighbour_node] = neighbour_node_distance + path_here
+                update_all_neighbours(neighbour_node, maze, path, size, 0)
+
+
+def find_path(maze, path, size):
+    steps = next_nodes(size)
+    for step in steps:
+        for node in step:
+            extend_neighbours(node, maze, path, size)
+    return path
+
+
+if __name__ == "__main__":
     p = Puzzle()
-    data = p.load_input()
-    data = p.load_input('input_test.txt')
+    data = p.load_input(string=True)
+    # data = p.load_input('input_test.txt', string=True)
+    # data = p.load_input('input_test2.txt', string=True)
+
+    size = len(data)
+    maze = zeros((size, size))
+    for i, line in enumerate(data):
+        x = fromstring(" ".join(list(line)), sep=" ")
+        maze[i, :] = x
+
+    path = zeros((size, size)) + 9999
+    path[(0, 0)] = 0
+
+    path = find_path(maze, path, size)
+    print(path)
+
+    # ========================
+    # part 2
+    size *= 5
+    path = zeros((size, size)) + 99999
+    path[(0, 0)] = 0
+
+    maze_row = maze
+    for i in range(1, 5):
+        new_maze = maze + 1
+        new_maze[new_maze == 10] = 1
+        maze = new_maze
+        maze_row = append(maze_row, new_maze, 1)
+    full_maze = maze_row
+    for j in range(1, 5):
+        new_maze = maze_row + 1
+        new_maze[new_maze == 10] = 1
+        maze_row = new_maze
+        full_maze = append(full_maze, new_maze, 0)
+
+    path = find_path(full_maze, path, size)
+    print(path)
--- AoC_2021/puzzle_18/puzzle_18.py
+++ AoC_2021/puzzle_18/puzzle_18.py
@@ -1,7 +1,78 @@
 from utils.puzzle import Puzzle
+import re
+
+# def add_numbers(number1, number2):
+#     return f'[{number1},{number2}]'
 
 
-if __name__ == '__main__':
+def left_right_number(text):
+    nests = 0
+    for i, ch in enumerate(text):
+        if ch == "[":
+            nests += 1
+            # if nests == 5:
+            #     explode = re.match(r'([0-9]+),([0-9]+)', raw_number[i+1:])
+            #     explode_l = int(explode[0])
+            #     explode_r = int(explode[1])
+            #     left = re.match(r'.*[,\[\]]([0-9]+)', raw_number[:i])
+            #     if left:
+            #         left_l = int(left[1])
+            #         left_part = raw_number[:left.start(1)] + str(explode_l + left_l) +
+            #
+            if nests > 5:
+                print(text)
+        elif ch == "]":
+            nests -= 1
+        elif ch == "," and nests == 1:
+            return text[1:i], text[i + 1 : -1]
+
+
+def parse_sn(text, parent):
+    sn = SnailfishNumber(parent)
+    left, right = left_right_number(text)
+    if len(left) != 1:
+        sn.left = parse_sn(left, sn)
+    else:
+        sn.left = int(left)
+
+    if len(right) != 1:
+        sn.right = parse_sn(right, sn)
+    else:
+        sn.right = int(right)
+    return sn
+
+
+class SnailfishNumber:
+    def __init__(self, parent, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+    # def max_
+    # def explode(self):
+
+
+
+
+if __name__ == "__main__":
     p = Puzzle()
-    data = p.load_input()
-    data = p.load_input('input_test.txt')
+    # data = p.load_input()
+    data = p.load_input("input_test.txt")
+
+    sn_list = []
+    for text in data:
+        sn_list.append(parse_sn(text, None))
+
+    for right in sn_list[1:]:
+        left = sn_list[0]
+        new_sn = SnailfishNumber(None)
+        left.parent = new_sn
+        right.parent = new_sn
+        new_sn.left = left
+        new_sn.right = right
+        sn_list[0] = new_sn
+
+        # TODO explode, split
+
+
