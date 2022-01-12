22ca9834fcb0e3bb26dec423437684f06c9b8f39
malmed <xmalmed@gmail.com>
2022-01-12 23:54:09 +0100

    Day 18, WIP

--- AoC_2021/puzzle_18/puzzle_18.py
+++ AoC_2021/puzzle_18/puzzle_18.py
@@ -1,58 +1,65 @@
 from utils.puzzle import Puzzle
-import re
-
-# def add_numbers(number1, number2):
-#     return f'[{number1},{number2}]'
 
 
-def left_right_number(text):
-    nests = 0
-    for i, ch in enumerate(text):
-        if ch == "[":
-            nests += 1
-            # if nests == 5:
-            #     explode = re.match(r'([0-9]+),([0-9]+)', raw_number[i+1:])
-            #     explode_l = int(explode[0])
-            #     explode_r = int(explode[1])
-            #     left = re.match(r'.*[,\[\]]([0-9]+)', raw_number[:i])
-            #     if left:
-            #         left_l = int(left[1])
-            #         left_part = raw_number[:left.start(1)] + str(explode_l + left_l) +
-            #
-            if nests > 5:
-                print(text)
-        elif ch == "]":
-            nests -= 1
-        elif ch == "," and nests == 1:
-            return text[1:i], text[i + 1 : -1]
+class Sn:
+
+    POWER = 5
+    SN = 2 ** POWER
+
+    def __init__(self):
+        self.d = [-1] * Sn.SN
+        self.v = [-1] * Sn.SN
+
+    def add(self, b):
+        c = Sn()
+        for i, d in enumerate(self.d):
+            if d != -1:
+                c.d[i // 2] = d + 1
+                c.v[i // 2] = self.v[i]
+        for j, d in enumerate(b.d):
+            if d != -1:
+                c.d[j // 2 + Sn.SN // 2] = d + 1
+                c.v[j // 2 + Sn.SN // 2] = b.v[j]
+        return c
+
+    def explode(self):
+        for i, d in enumerate(self.d):
+            if d == Sn.POWER:
+                v1 = self.v[i]
+                v2 = self.v[i + 1]
+                self.v[i] = 0
+                self.v[i + 1] = -1
+                self.d[i] += -1
+                self.d[i + 1] = -1
+                for j in range(i).__reversed__():
+                    if self.d[j] != -1:
+                        self.v[j] += v1
+                        break
+                for k in range(i + 2, Sn.SN):
+                    if self.d[k] != -1:
+                        self.v[k] += v2
+                        break
 
 
-def parse_sn(text, parent):
-    sn = SnailfishNumber(parent)
-    left, right = left_right_number(text)
-    if len(left) != 1:
-        sn.left = parse_sn(left, sn)
-    else:
-        sn.left = int(left)
+def load_sn(text):
+    sn_list = eval(text)
+    sn = Sn()
 
-    if len(right) != 1:
-        sn.right = parse_sn(right, sn)
-    else:
-        sn.right = int(right)
+    def find_leaf(sn_list, path):
+        for j in range(2):
+            if isinstance(sn_list[j], int):
+                depth = len(path) + 1
+                new_path = path + str(j)
+                while len(new_path) < Sn.POWER:
+                    new_path += "0"
+                index = int(new_path, 2)
+                sn.d[index] = depth
+                sn.v[index] = sn_list[j]
+            else:
+                find_leaf(sn_list[j], path + str(j))
+
+    find_leaf(sn_list, "")
     return sn
-
-
-class SnailfishNumber:
-    def __init__(self, parent, left=None, right=None):
-        self.left = left
-        self.right = right
-        self.parent = parent
-
-
-    # def max_
-    # def explode(self):
-
-
 
 
 if __name__ == "__main__":
@@ -60,19 +67,20 @@ if __name__ == "__main__":
     # data = p.load_input()
     data = p.load_input("input_test.txt")
 
-    sn_list = []
-    for text in data:
-        sn_list.append(parse_sn(text, None))
+    first = load_sn(data[0])
+    second = load_sn(data[1])
+    ab = first.add(second)
+    print(ab.v)
+    print(ab.d)
 
-    for right in sn_list[1:]:
-        left = sn_list[0]
-        new_sn = SnailfishNumber(None)
-        left.parent = new_sn
-        right.parent = new_sn
-        new_sn.left = left
-        new_sn.right = right
-        sn_list[0] = new_sn
+    print()
+    c = load_sn(data[2])
+    print(c.v)
+    print(c.d)
+    print()
 
-        # TODO explode, split
+    c.explode()
+    print(c.v)
+    print(c.d)
 
-
+    print()
