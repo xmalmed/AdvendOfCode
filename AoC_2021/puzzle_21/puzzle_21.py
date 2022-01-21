f5ba5efe7b042876690a632df69ba136bba6a29e
malmed <xmalmed@gmail.com>
2022-01-21 22:39:31 +0100

    Day 21, part 2

--- AoC_2021/puzzle_21/puzzle_21.py
+++ AoC_2021/puzzle_21/puzzle_21.py
@@ -1,6 +1,32 @@
 from utils.puzzle import Puzzle
 from parse import parse
 
+
+def count_turn(games):
+    wins, cont = 0, 0
+    next_games = []
+    for (p, s, n) in games:
+        for k, v in DICE3.items():
+            np = (p + k - 1) % 10 + 1
+            ns = s + np
+            nn = n * v
+            if ns >= 21:
+                wins += nn
+            else:
+                cont += nn
+                next_games.append((np, ns, nn))
+    return wins, cont, next_games
+
+
+def count_games_of_player(start_position):
+    games = [(start_position, 0, 1)]
+    spaces = []
+    while games:
+        wins, cont, games = count_turn(games)
+        spaces.append((wins, cont))
+    return spaces
+
+
 if __name__ == "__main__":
     p = Puzzle()
     data = p.load_input()
@@ -28,3 +54,20 @@ if __name__ == "__main__":
             winner = True
         whose_turn = (whose_turn + 1) % len(players)
 
+    # part 2
+    DICE3 = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+    spaces1 = count_games_of_player(p1)
+    spaces2 = count_games_of_player(p2)
+
+    p1_wins = 0
+    for i, w in enumerate(spaces1):
+        if i == 0:
+            continue
+        p1_wins += w[0] * spaces2[i - 1][1]
+
+    p2_wins = 0
+    for i, w in enumerate(spaces2):
+        p2_wins += w[0] * spaces1[i][1]
+
+    print(p1_wins)
+    print(p2_wins)
