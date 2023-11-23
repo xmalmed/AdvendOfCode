70bf286c77178b43cb1632c50c29a6e6076703b5
malmed <xmalmed@gmail.com>
2023-11-23 19:02:22 +0100

    d19

--- AoC_2021/puzzle_19/puzzle_19.py
+++ AoC_2021/puzzle_19/puzzle_19.py
@@ -1,58 +1,67 @@
 from utils.puzzle import Puzzle
 from re import match
+import numpy as np
 
-
-def get_distances(scan):
-    distances = []
-    for i in range(len(scan) - 1):
-        for j in range(i + 1, len(scan)):
-            d = (
-                (scan[i][0] - scan[j][0]) ** 2
-                + (scan[i][1] - scan[j][1]) ** 2
-                + (scan[i][2] - scan[j][2]) ** 2
-            )
-            distances.append((i, j, d))
-    return distances
+RZ = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+RY = np.array([[0, 0, 1], [0, 1, 0], [-1, 0, 0]])
+RX = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
 
 
 def is_overlaped(scan1, scan2):
-    d1 = set(d[2] for d in get_distances(scan1))
-    d2 = set(d[2] for d in get_distances(scan2))
-    overlap = d1.intersection(d2)
-    return True if len(overlap) >= (12*11/2) else False
+    overlap = scan1.distance_set.intersection(scan2.distance_set)
+    return len(overlap)
 
 
-def align(scan1, scan2):
-    d1 = get_distances(scan1)
-    d2 = get_distances(scan2)
-    for d in d1:
-        i = None
-        try:
-            i = [dd[2] for dd in d2].index(d)
-        except ValueError:
-            pass
-        if i:
-            break
-    # s1 d
-    # d2[i]
-
-class Scan():
-    def __init__(self, coordinates: list):
-        self.coordinates = coordinates
-        self.start_directions = self.initial_directions()
+# def align(scan1, scan2):
+#     d1 = get_distances(scan1)
+#     d2 = get_distances(scan2)
+#     for d in d1:
+#         i = None
+#         try:
+#             i = [dd[2] for dd in d2].index(d)
+#         except ValueError:
+#             pass
+#         if i:
+#             break
+#     # s1 d
+#     # d2[i]
 
 
-    def initial_directions(self):
-        start_directionsnces = {}
-        for i in range(len(self.coordinates) - 1):
-            for j in range(i + 1, len(self.coordinates)):
-                v = (
-                        (self.coordinates[j][0] - self.coordinates[i][0]),
-                        + (self.coordinates[j][1] - self.coordinates[i][1]),
-                        + (self.coordinates[j][2] - self.coordinates[i][2]),
-                )
-                start_directionsnces[(i, j)] = v
-        return start_directionsnces
+class Scanner:
+
+    def __init__(self, beacons: list):
+        self.beacons = beacons
+        self.distance_net = self.calculate_net_distances()
+        self.distance_set = set(self.distance_net.values())
+
+    def calculate_net_distances(self):
+        distances = {}
+        for i in range(len(self.beacons) - 1):
+            for j in range(i + 1, len(self.beacons)):
+                d = np.dot((self.beacons[i] - self.beacons[j]), (self.beacons[i] - self.beacons[j]))
+                distances[(i, j)] = d
+        return distances
+
+    def rotateZ(self):
+        self.beacons = [RZ @ b for b in self.beacons]
+
+    def rotateY(self):
+        self.beacons = [RY @ b for b in self.beacons]
+
+    def rotateX(self):
+        self.beacons = [RX @ b for b in self.beacons]
+
+    # def initial_directions(self):
+    #     start_directionsnces = {}
+    #     for i in range(len(self.coordinates) - 1):
+    #         for j in range(i + 1, len(self.coordinates)):
+    #             v = (
+    #                     (self.coordinates[j][0] - self.coordinates[i][0]),
+    #                     + (self.coordinates[j][1] - self.coordinates[i][1]),
+    #                     + (self.coordinates[j][2] - self.coordinates[i][2]),
+    #             )
+    #             start_directionsnces[(i, j)] = v
+    #     return start_directionsnces
 
 
 if __name__ == "__main__":
@@ -66,12 +75,14 @@ if __name__ == "__main__":
         if match(r"--- scanner", line):
             scanner = []
         elif line == "":
-            scanners.append(Scan(scanner))
+            scanners.append(Scanner(scanner))
             scanner = []
         else:
-            scanner.append(tuple(int(x) for x in line.split(",")))
+            scanner.append(np.array([int(x) for x in line.split(",")]))
     if scanner:
-        scanners.append(Scan(scanner))
+        scanners.append(Scanner(scanner))
 
 
-    print(scanners[0].initial_directions())
+    print(is_overlaped(scanners[0], scanners[1]))
+
+    print()
